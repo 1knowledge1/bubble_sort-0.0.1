768b6a5591ebdbf19624eec314480246e321b0c9
Yaroslav <32066631+1knowledge1@users.noreply.github.com>
2017-11-10 08:38:06 +0300

    Update bubsrt@0.0.1.cpp

--- bubsrt@0.0.1.cpp
+++ bubsrt@0.0.1.cpp
@@ -1,1 +1,62 @@
+#include <iostream>
+#include <sstream>
 
+using namespace std;
+
+bool input(float * &elements, unsigned int num)
+{
+    float *tmp_elements;
+    string str;
+
+    getline(cin, str);
+
+    tmp_elements = new float [num];
+
+    for (string str; getline(cin, str); ) {
+        std::istringstream stream(str);
+        for (unsigned int j = 0; j < num; ++j) {
+            if(!(stream >> tmp_elements[j])) {
+                return false;
+            }
+        }
+        break;
+    }
+    elements = tmp_elements;
+    return true;
+}
+
+void bubsort(float *elements, unsigned int num)
+{
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num - i - 1; j++) {
+            if (elements[j] > elements[j + 1]) {
+
+                swap(elements[j], elements[j + 1]);
+            }
+        }
+    }
+
+}
+
+int main() {
+    float *elements;
+    unsigned int num;
+
+    if(!(cin>>num))
+    {
+        cout <<"An error has occured while reading input data.\n";
+        return 0;
+    }
+
+    if(!(input(elements,num)))
+    {
+        cout <<"An error has occured while reading input data.\n";
+        return 0;
+    }
+
+    bubsort(elements, num);
+    for (int i = 0; i < num; i++) cout << elements[i]<<' ';
+    
+    delete[] elements;
+    return 0;
+}
